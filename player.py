8e4c9b332876b6636be3ddd9b11fc8b0542c8e72
Dean <deang467@gmail.com>
2022-01-06 08:37:01 -0500

    Update player.py

--- player.py
+++ player.py
@@ -17,33 +17,6 @@ class Player:
         """
         self.score = 0
         self.hand = []
-    
-    def calc_score(self, card: int) -> None:
-        """Calculates the player's current score
-
-        Returns:
-            int: the score
-        """
-        self.score += card
-    
-    def draw_card(self) -> None:
-        """Draws a random card from the deck
-
-        Returns:
-            int: a randomly selected card
-        """
-        deck = [2,3,4,5,6,7,8,9,10,10,10,10,11]
-        card = random.choice(deck)
-        self.hand.append(card)
-        self.calc_score(card)
-        if self.score > 21 and card == 11:
-            self.eleven()
-         
-    
-    def eleven(self) -> None:
-        self.hand.remove(11)
-        self.hand.append(1)
-        self.score -= 10
         
 class Dealer(Player):
     """
@@ -55,9 +28,14 @@ class Dealer(Player):
     """
     def __init__(self) -> None:
         super().__init__()
+        deck = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,
+                9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,11,11,11,11]
         while self.score < 17:
-            card = self.draw_card()
-            if card == 11 and self.score > 17:
-                card = 1
-                self.score -= 11
-                self.score += 1
+            card = random.choice(deck)
+            self.hand.append(card)
+            deck.remove(card)
+            self.score += card
+            if self.score > 17 and card == 11:
+                self.score += -10
+                self.hand.remove(11)
+                self.hand.append(1)
